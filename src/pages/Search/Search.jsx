d3e329db56606e064722c2ad7dfb57e18e3bb0a3
mahdi-baghshahi <mahdibaghshahi81@gmail.com>
2025-11-11 14:22:06 +0330

    commit search pages

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import styles from './Search.module.css';
+import SearchNavBar from '../../components/common/SearchNavBar/SearchNavBar';
+import Styledtext from '../../components/common/styled-text/Styledtext';
+import { Cheveron_left } from '../../components/Icons/Icons';
+import { Link } from 'react-router-dom';
 
 export default function Search() {
-  return (
-    <div>Search</div>
-  )
+ 
+    return (
+      <>
+      <nav className={styles.navbark}>
+        <Link to={'/home0notes'}>
+           <div >
+              <Cheveron_left /> 
+            </div>
+        </Link>
+        <SearchNavBar />
+      </nav>
+      <section className={styles.sectionSearch}>
+         <div>
+            <Styledtext variant={'p3'} >RECENT SEARCHES</Styledtext>
+         </div>
+      </section>
+      </>
+    );
 }
